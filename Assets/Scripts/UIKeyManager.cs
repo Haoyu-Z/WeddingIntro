Haoyu-Z/WeddingIntro

commit e304def0bf9c46754eb3a6a10d01e9a655ad7558

NS face button feature (bug:cannot do single-press)

Assets/Scripts/UIKeyManager.cs
@@ -1,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIKeyCode
+{
+    UI_Up, 
+    UI_Down,
+    UI_Left,
+    UI_Right,  
+    UI_A,
+    UI_B,
+}
+
+public struct UIKeyState
+{
+    public UIKeyCode keyCode;
+    public bool IsPressed;
+    public bool IsPressedLastFrame;
+
+    public UIKeyState(UIKeyCode key, bool pressed, bool pressedLastFrame = false)
+    {
+        keyCode = key;
+        IsPressed = pressed;
+        IsPressedLastFrame = pressedLastFrame;
+    }
+}
+
+public class UIKeyManager : MonoBehaviour
+{
+    private Dictionary<UIKeyCode, UIKeyState> keyStates = new Dictionary<UIKeyCode, UIKeyState>();
+
+    public void SetKeyState(UIKeyCode key, bool state)
+    {
+        if (!keyStates.ContainsKey(key))
+        {
+            keyStates.Add(key, new UIKeyState(key, state));
+        }
+        else
+        {
+            keyStates[key] = new UIKeyState(key, state, keyStates[key].IsPressedLastFrame);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        foreach (UIKeyCode key in keyStates.Keys)
+        {
+            UIKeyState state = keyStates[key];
+            state.IsPressedLastFrame = state.IsPressed;
+        }
+    }
+
+    public bool GetUIKey(UIKeyCode key)
+    {
+        return keyStates.ContainsKey(key) && keyStates[key].IsPressed;
+    }
+
+    public bool GetUIKeyDown(UIKeyCode key)
+    {
+        return GetUIKey(key) && !keyStates[key].IsPressedLastFrame;
+    }
+}

Assets/Data/InputSettings.asset
@@ -14,17 +14,22 @@ MonoBehaviour:
   m_EditorClassIdentifier: 
   InputSettings:
   - key: 119
+    uiKey: 0
     keyDefinition: 0
     keyType: 0
   - key: 115
+    uiKey: 1
     keyDefinition: 1
     keyType: 0
   - key: 97
+    uiKey: 2
     keyDefinition: 2
     keyType: 0
   - key: 100
+    uiKey: 3
     keyDefinition: 3
     keyType: 0
   - key: 106
+    uiKey: 4
     keyDefinition: 4
     keyType: 1

Assets/Prefabs/BP_GameStatics.prefab
@@ -11,6 +11,7 @@ GameObject:
   - component: {fileID: 8931848893862806467}
   - component: {fileID: 8931848893862806468}
   - component: {fileID: 8931848893862806469}
+  - component: {fileID: 1927697592781912968}
   m_Layer: 0
   m_Name: BP_GameStatics
   m_TagString: Untagged
@@ -60,3 +61,15 @@ MonoBehaviour:
   m_EditorClassIdentifier: 
   AllDialogs:
   - {fileID: 11400000, guid: 4de474d6144d54207a30437904b6019f, type: 2}
+--- !u!114 &1927697592781912968
+MonoBehaviour:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 8931848893862806470}
+  m_Enabled: 1
+  m_EditorHideFlags: 0
+  m_Script: {fileID: 11500000, guid: 97208fd39bcf4eb46bcff45787af6ee8, type: 3}
+  m_Name: 
+  m_EditorClassIdentifier: 

Assets/Scripts/AvatarInput.cs
@@ -46,6 +46,7 @@ public enum KeyPressType
 public struct GameKeyDefinition
 {
     public KeyCode key;
+    public UIKeyCode uiKey;
     public GameKeyCode keyDefinition;
     public GameKeyType keyType;
 }
@@ -107,10 +108,14 @@ public class AvatarInput : MonoBehaviour
 
     private void Update()
     {
+        Debug.Assert(GameStatics.Instance != null || GameStatics.Instance.UIKeyManager != null);
+
+        UIKeyManager uiKeyManager = GameStatics.Instance.UIKeyManager;
+
         foreach (GameKeyDefinition definition in inputSetting.InputSettings)
         {
-            bool keyDown = Input.GetKeyDown(definition.key);
-            bool keyPressing = Input.GetKey(definition.key);
+            bool keyDown = Input.GetKeyDown(definition.key) || uiKeyManager.GetUIKeyDown(definition.uiKey);
+            bool keyPressing = Input.GetKey(definition.key) || uiKeyManager.GetUIKey(definition.uiKey);
             if (keyDown || keyPressing)
             {
                 Debug.Assert(gameKeyTypeToPriorityType.ContainsKey(definition.keyType));

Assets/Scripts/GameStatics.cs
@@ -44,20 +44,26 @@ public class GameStatics : MonoBehaviour
 
     private AvatarMovementComponent playerAvatarMovement;
 
-    public AvatarMovementComponent PlayerAvatarMovement { get { return playerAvatarMovement; } }
+    public AvatarMovementComponent PlayerAvatarMovement => playerAvatarMovement;
 
     private AvatarInteraction playerAvatarInteraction;
 
-    public AvatarInteraction PlayerAvatarInteraction { get { return playerAvatarInteraction; } }
+    public AvatarInteraction PlayerAvatarInteraction => playerAvatarInteraction;
 
     private AvatarInput playerAvatarInput;
 
-    public AvatarInput PlayerAvatarInput { get { return playerAvatarInput; } }
+    public AvatarInput PlayerAvatarInput => playerAvatarInput;
+
+    private UIKeyManager uiKeyManager;
+
+    public UIKeyManager UIKeyManager => uiKeyManager;
 
     private void Awake()
     {
         Debug.Assert(Instance == null);
         Instance = this;
+
+        uiKeyManager = GetComponent<UIKeyManager>();    
 
         if (PlayerAvatar != null)
         {

Assets/Scripts/UIButtonClicker.cs
@@ -1,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonClicker : MonoBehaviour
+{
+    [SerializeField]
+    private UIKeyCode key;
+
+    public void OnPressed()
+    {
+        Debug.Assert(GameStatics.Instance != null && GameStatics.Instance.UIKeyManager != null);
+        GameStatics.Instance.UIKeyManager.SetKeyState(key, true);
+    }
+
+    public void OnRelease()
+    {
+        Debug.Assert(GameStatics.Instance != null && GameStatics.Instance.UIKeyManager != null);
+        GameStatics.Instance.UIKeyManager.SetKeyState(key, false);
+    }
+}

Assets/Scripts/UIButtonClicker.cs.meta
@@ -1,0 +1,11 @@
+fileFormatVersion: 2
+guid: cdf3a1c9dfd323d468156ac367a77eee
+MonoImporter:
+  externalObjects: {}
+  serializedVersion: 2
+  defaultReferences: []
+  executionOrder: 0
+  icon: {instanceID: 0}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 

commit 9b3bd9ff60df10ddc27bdeef0fc1f7c2ce75bdfe

fix bug introduced by e304def0bf9c46754eb3a6a10d01e9a655ad7558

Assets/Scripts/UIKeyManager.cs
@@ -12,7 +12,7 @@ public enum UIKeyCode
     UI_B,
 }
 
-public struct UIKeyState
+public class UIKeyState
 {
     public UIKeyCode keyCode;
     public bool IsPressed;
@@ -38,7 +38,7 @@ public class UIKeyManager : MonoBehaviour
         }
         else
         {
-            keyStates[key] = new UIKeyState(key, state, keyStates[key].IsPressedLastFrame);
+            keyStates[key].IsPressed = state;
         }
     }
 